9e942b96ca3da42f57a366608fce6f3ca64e1a1d
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-08-16 18:13:46 -0600

    Linear search algorithm

---
+++ Dsa_CSharp/Dsa_CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsa_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>() { 5, 10, 15, 20, 25, 30 };
+            Console.WriteLine(Program.linearSearch(ints, 35));
+
+            Console.ReadLine();
+        }
+
+        // Linear search algorithm. Less efficient on larger data sets.
+        static int linearSearch(List<int> elements, int x)
+        {
+            int length = elements.Count;
+            for(int i = 0; i < length; i++)
+            {
+                if (elements[i] == x)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
