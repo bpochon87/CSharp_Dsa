92c2db47abf55336ba5158d75539d6d1d8f29c91
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-08-18 16:18:42 -0600

    New algorithms

--- Dsa_CSharp/Dsa_CSharp/Program.cs
+++ Dsa_CSharp/Dsa_CSharp/Program.cs
@@ -6,16 +6,48 @@ using System.Threading.Tasks;
 
 namespace Dsa_CSharp
 {
+
+    // This class is referenced by our DepthFirstSearchTraversalOnTree method.
+    public class Node
+    {
+        public Node left;
+        public Node right;
+        public int data;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            // C# built-in binary search algo
-            int[] example = new int[] { 1, 3, 5, 6, 8, 11 };
-            Console.WriteLine(Array.BinarySearch(example, 6));
+            //// C# built-in binary search algo
+            //int[] example = new int[] { 1, 3, 5, 6, 8, 11 };
+            //Console.WriteLine(Array.BinarySearch(example, 6));
 
-            List<int> ints = new List<int>() { 5, 10, 15, 20, 25, 30 };
-            Console.WriteLine(Program.interpolationSearch(example, 6, 3));
+            //List<int> ints = new List<int>() { 5, 10, 15, 20, 25, 30 };
+            //Console.WriteLine(Program.interpolationSearch(example, 6, 3));
+
+            // Tree traversal algorithms.
+            Node leftNode = new Node()
+            {
+                data = 1,
+                left = null,
+                right = null
+            };
+
+            Node rightNode = new Node()
+            {
+                data = 3,
+                left = null,
+                right = null,
+            };
+
+            Node root = new Node()
+            {
+                data = 2,
+                left = leftNode,
+                right = rightNode
+            };
+            PostOrder(root);
 
             Console.ReadLine();
         }
@@ -69,9 +101,9 @@ namespace Dsa_CSharp
         {
             int low = 0;
             int high = length - 1;
-            while(low <= high && x >= arr[low] && x <= arr[high])
+            while (low <= high && x >= arr[low] && x <= arr[high])
             {
-                if(low == high)
+                if (low == high)
                 {
                     if (arr[low] == x)
                     {
@@ -101,6 +133,163 @@ namespace Dsa_CSharp
                 }
             }
             return -1;
+        }
+
+        // Depth-first tree traversal algo
+        public void DepthFirstSearchTraversalOnTree(Node root)
+        {
+            // Initializing new Stack of Nodes
+            Stack<Node> nodes = new Stack<Node>();
+            nodes.Push(root);
+            while (nodes.Count > 0)
+            {
+                Node node = nodes.Pop();
+
+                if (node.right != null)
+                {
+                    nodes.Push(node.right);
+                }
+
+                if (node.left != null)
+                {
+                    nodes.Push(node.left);
+                }
+
+                Console.WriteLine(" " + node.data);
+            }
+        }
+
+        //// Depth-first graph traversal algo
+        //// Non-working due to never initializing graph.
+        //public HashSet<int> DepthFirstSearchTraversalOnGraph(Node root)
+        //{
+        //    HashSet<int> visited = new HashSet<int>();
+
+        //    // Say: if root node is not in the adjacency list...
+        //    if (!graph.AdjacencyList.ContainsKey(root))
+        //    {
+        //        return visited;
+        //    }
+
+        //    Stack<int> stack = new Stack<int>();
+        //    stack.Push(root.data);
+
+        //    while (stack.Count > 0)
+        //    {
+        //        int vertex = stack.Pop();
+
+        //        if (visited.Contains(vertex))
+        //        {
+        //            continue;
+        //        }
+
+        //        visited.Add(vertex);
+
+        //        foreach (var neighbor in graph.AdjacencyList[vertex])
+        //        {
+        //            // Say: if the visited HashSet does not contain neighbor...
+        //            if (!visited.Contains(neighbor))
+        //            {
+        //                stack.Push(neighbor);
+        //            }
+        //        }
+
+        //        return visited;
+        //    }
+        //}
+
+        // Breadth-first tree traversal algo
+        // Using Node class from top
+        public void BreadthFirstSearchTraversalOnTree(Node root)
+        {
+            Queue<Node> nodes = new Queue<Node>();
+            if (root == null)
+            {
+                return;
+            }
+
+            nodes.Enqueue(root);
+            while (nodes.Count > 0)
+            {
+                Node node = nodes.Dequeue();
+                Console.WriteLine(" " + node.data);
+                if (node.left != null)
+                {
+                    nodes.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    nodes.Enqueue(node.right);
+                }
+            }
+        }
+
+        ////Breadth-first graph traversal algo
+        ////Non-working as graph is never initialized.
+        //public HashSet<int> BreadthFirstTraversalOnGraph(Node root)
+        //{
+        //    HashSet<int> visited = new HashSet<int>();
+
+        //    if (!graph.AdjacencyList.ContainsKey(root))
+        //    {
+        //        return visited;
+        //    }
+
+        //    Queue<int> q = new Queue<int>();
+        //    q.Enqueue(root.data);
+        //    while (q.Count > 0)
+        //    {
+        //        int vertex = q.Dequeue();
+
+        //        if (visited.Contains(vertex))
+        //        {
+        //            continue;
+        //        }
+
+        //        visited.Add(vertex);
+
+        //        foreach (var neighbor in graph.AdjacencyList[vertex])
+        //        {
+        //            if (!visited.Contains(neighbor))
+        //            {
+        //                q.Enqueue(neighbor);
+        //            }
+        //        }
+        //    }
+        //return visited;
+        //}
+
+        // In-order tree traversal algo
+        static void InOrder(Node root)
+        {
+            if(root != null)
+            {
+                InOrder(root.left);
+                Console.WriteLine(root.data);
+                InOrder(root.right);
+            }
+        }
+
+        // Pre-order tree traversal algo
+        static void PreOrder(Node root)
+        {
+            if(root != null)
+            {
+                Console.WriteLine(root.data);
+                PreOrder(root.left);
+                PreOrder(root.right);
+            }
+        }
+
+        // Post-order tree traversal algo
+        static void PostOrder(Node root)
+        {
+            if(root != null)
+            {
+                PostOrder(root.left);
+                PostOrder(root.right);
+                Console.WriteLine(root.data);
+            }
         }
     }
 }
